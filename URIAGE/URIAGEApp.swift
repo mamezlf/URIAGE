8b04132478266f80c2cafb3f2214a9292cac7d2c
mamezlf <mamezhlf@gmail.com>
2026-04-26 02:38:57 +0900

    Initial Commit

---
+++ URIAGE/URIAGEApp.swift
@@ -0,0 +1,17 @@
+//
+//  URIAGEApp.swift
+//  URIAGE
+//
+//  Created by zlf on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct URIAGEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
